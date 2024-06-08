951b21b226ad1836c417dcea34ecb52a1f3b32c6
Jacob Campbell <jacobcampbell04@protonmail.com>
2024-06-08 10:06:16 +0100

    Registered the main app that will be used for this project

--- blockwardrobe/blockwardrobe/settings.py
+++ blockwardrobe/blockwardrobe/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blockwardrobeapp.apps.BlockwardrobeappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
