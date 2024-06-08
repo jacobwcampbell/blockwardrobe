3741520af0972575d36b2bdd36fc26cc49384841
Jacob Campbell <jacobcampbell04@protonmail.com>
2024-06-08 10:33:43 +0100

    Added image to model with debug setting to view it

--- blockwardrobe/blockwardrobeapp/models.py
+++ blockwardrobe/blockwardrobeapp/models.py
@@ -5,3 +5,4 @@ class Skin(models.Model):
     name = models.CharField(max_length=20)
     description = models.TextField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='/skins/default.png', upload_to='skins')
