b3498178a2fa3c7b3b31285939cdadf02bc0a1da
Jacob Campbell <jacobcampbell04@protonmail.com>
2024-06-08 15:39:31 +0100

    Implemented detail view and standardised basic html

--- blockwardrobe/blockwardrobeapp/urls.py
+++ blockwardrobe/blockwardrobeapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'blockwardrobeapp'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('skin/<int:pk>/', views.SkinView.as_view(), name='skin'),
 ]
--- blockwardrobe/blockwardrobeapp/views.py
+++ blockwardrobe/blockwardrobeapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 from .models import Skin
 
+class SkinView(generic.DetailView):
+    model = Skin
+    template_name = 'blockwardrobeapp/skin.html'
+
 def index(request):
     return render(request, 'blockwardrobeapp/index.html', {'all_skins': Skin.objects.all()})
+
